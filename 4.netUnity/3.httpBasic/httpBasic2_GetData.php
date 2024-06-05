079b6a3e67613d4692b8c9ee73cf13440537a555
GoToSchoolTeams <schoolgoto@gmail.com>
2024-06-05 10:39:31 +0900

    del

--- 4.netUnity/3.httpBasic/httpBasic2_GetData.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*Getで送信されたデータを受け取る*/
-$first = $_GET["first"];
-$second = $_GET["second"];
-//表示
-echo $first . "</br>";
-echo $second . "</br>";
-?>
--- 4.netUnity/3.httpBasic/httpBasic3_PostData.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*Postで送信されたデータを受け取る*/
-$first = $_POST["first"];
-$second = $_POST["second"];
-//表示
-echo $first . "</br>";
-echo $second . "</br>";
-?>
